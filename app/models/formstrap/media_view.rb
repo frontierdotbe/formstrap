c4e57a2e34968f34863ee7e31a6cf53bd53a7129
Jef Vlamings <vlamingsjef@gmail.com>
2024-02-08 16:33:49 +0100

    Fix edit url issue'

--- app/models/formstrap/media_view.rb
+++ app/models/formstrap/media_view.rb
@@ -130,7 +130,7 @@ module Formstrap
     def edit_modal_url(attachment)
       return nil unless edit_url.present?
       return edit_url unless attachment&.persisted?
-      edit_url.gsub(":id", attachment.id.to_s)
+      edit_url.gsub(":id", attachment.blob&.id.to_s)
     end
 
     attr_reader :accept
