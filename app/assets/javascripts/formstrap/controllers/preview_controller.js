2e147b45a5b88faf9aa0e678c9eebed96d8f817e
Jef Vlamings <vlamingsjef@gmail.com>
2024-03-05 20:23:46 +0100

    run eslint

--- app/assets/javascripts/formstrap/controllers/preview_controller.js
+++ app/assets/javascripts/formstrap/controllers/preview_controller.js
@@ -1,22 +1,22 @@
-import {Controller} from '@hotwired/stimulus'
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   static values = {
     url: String
   }
 
-  connect() {
-    this.button = this.element;
+  connect () {
+    this.button = this.element
     this.button.addEventListener('click', (event) => {
       event.preventDefault()
-      this.handleButtonClick();
+      this.handleButtonClick()
     })
   }
 
-  handleButtonClick() {
-    const form = this.form().cloneNode(true);
-    const authenticityTokenInput = form.querySelector('input[name="authenticity_token"]');
-    const newAuthenticityToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+  handleButtonClick () {
+    const form = this.form().cloneNode(true)
+    const authenticityTokenInput = form.querySelector('input[name="authenticity_token"]')
+    const newAuthenticityToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
     const methodInput = form.querySelector('input[name="_method"]')
 
     // Remove [id] fields
@@ -26,22 +26,22 @@ export default class extends Controller {
     })
 
     // Override the form and submit
-    form.setAttribute("action", this.urlValue);
-    form.setAttribute("target", "_blank");
+    form.setAttribute('action', this.urlValue)
+    form.setAttribute('target', '_blank')
     authenticityTokenInput.value = newAuthenticityToken
-    methodInput.value = "post"
+    methodInput.value = 'post'
 
     // Insert form in DOm
     document.body.appendChild(form)
 
     // Submit form
-    form.submit();
+    form.submit()
 
     // Remove form
     document.body.removeChild(form)
   }
 
-  form() {
-    return this.button.closest('form');
+  form () {
+    return this.button.closest('form')
   }
 }
