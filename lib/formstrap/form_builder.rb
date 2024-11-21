714c8a55e0c0045010630297b477e7e924c8225d
Gert-Jan Peeters <gertjanpeeters@gmail.com>
2024-11-21 15:57:06 +0100

    Form submitted when pressing enter. (#65)
    * Prototype.
    
    * Form gets submitted when pressed enter.
    
    ---------
    
    Co-authored-by: Jef Vlamings <vlamingsjef@gmail.com>

--- lib/formstrap/form_builder.rb
+++ lib/formstrap/form_builder.rb
@@ -11,15 +11,15 @@ module Formstrap
       render_input(:association, attribute, options)
     end
 
-    def preview_button(value = nil, options = {}, &block)
+    def preview_button(url = nil, options = {}, &block)
       default_options = {
         data: {
           controller: "preview",
-          "preview-url-value": options[:url]
-        },
-        type: nil
+          preview_url_value: url
+        }
       }
-      button value, default_options.deep_merge(options.except(:url)), &block
+
+      @template.render("formstrap/link", form: self, url: url, options: default_options.deep_merge(options), &block)
     end
 
     def checkbox(attribute, formstrap: true, **options)
